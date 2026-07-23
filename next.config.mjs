a040f2a489f023d4490150f8c368e21686021fa9
ypolash <yapbit3@gmail.com>
2026-07-23 17:00:42 +0600

    Fix: Remove unsupported eslint config for Next.js 16

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+
   typescript: {
     ignoreBuildErrors: true,
   },

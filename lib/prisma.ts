c232f1231aa63f34889b5c95c4394008b0e5ac39
ypolash <yapbit3@gmail.com>
2026-07-26 01:21:06 +0600

    fix: Add default export to prisma client for Next.js build

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -5,3 +5,5 @@ const globalForPrisma = global as unknown as { prisma_v2: PrismaClient }
 export const prisma = globalForPrisma.prisma_v2 || new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma_v2 = prisma
+
+export default prisma;

06f4d413a1e6c93dce6416dbd345715d1341e5ce
ypolash <yapbit3@gmail.com>
2026-08-20 17:35:34 +0600

    Auto-save: Deleted legacy CRM folders from dashboard as requested

--- app/dashboard/crm/reports/components/ActivityTimeline.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import React from 'react';
-import { ActivityFeed } from './ActivityFeed';
-
-// A wrapper to reuse ActivityFeed for simpler dashboard views
-export function ActivityTimeline() {
-  return <ActivityFeed />;
-}
